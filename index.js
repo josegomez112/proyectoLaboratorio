1d04d0e05d54f00eca363c893ba60fc31920329d
Diego Apugliese <dapugliese@gmail.com>
2025-07-15 21:05:23 -0300

    versión 1.0

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const sql = require('mssql');
+const app = express();
+app.use(express.json());
+
+
+const config = {
+  user: 'GRUPO01',
+  password: 'Grupo01',
+  server: '10.120.3.250\SQLEXPRESS',
+  database: 'tu_base_de_datos',
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};
+
+
+
+app.use(express.static('public'));
+
+//app.use(cors()); // Permite todas las solicitudes desde cualquier origen
+
+app.get('/api/datos', async (req, res) => {
+  try {
+    await sql.connect(config);
+    const result = await sql.query('SELECT * FROM tu_tabla');
+    res.json(result.recordset);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
